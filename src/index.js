1990801cf0b7c0c89c1c9ba635e2447d1566e7e9
Arnav Singh <arnav010singh@gmail.com>
2023-08-06 00:16:53 +0530

    minor fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-// import "tailwindcss/tailwind.css";
+import "tailwindcss/tailwind.css";
 import { Provider } from "react-redux";
 import store from "./store/index";
 
